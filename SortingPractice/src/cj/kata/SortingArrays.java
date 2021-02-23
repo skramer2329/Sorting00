c24534b00c99d2cd2756f44b052b6c308fb31185
skramer2329 <skramer2329@gmail.com>
2021-02-22 22:20:18 -0600

    Building Test Class, added some notes to SortingArrays

--- SortingPractice/src/cj/kata/SortingArrays.java
+++ SortingPractice/src/cj/kata/SortingArrays.java
@@ -1,5 +1,48 @@
 package cj.kata;
-
+/*
+ * Sabrina Kramer
+ * Personal Project
+ * Sorting
+ * 
+ */
 public class SortingArrays {
-
+	//Different sorting algorithms to code
+		//Quick sort
+		//Bubble sort
+		//Merge sort
+		//Insertion sort
+		//Selection sort
+		//Heap sort
+		//Radix sort
+		//Bucket sort
+	
+	
+	//Data Structures to work with
+		//Arrays, Linked Lists, BST
+	
+	
+	//Other Projects I want to work on (will make separate note later)
+		//Queue, Stack, Heap, Lists, Iterators 
+	
+	
+	
+	
+	/*
+	 * TODO 
+	 * Need to figure out what kinds of things I want this class to do
+	 * For now it will only be sorting data structures
+	 */
+	
+	/*
+	 * TODO
+	 * Need to create other classes that will sort different kinds of 
+	 * data structures. These classes will need their own test classes.
+	 */
+	
+	/*
+	 * TODO
+	 * Later, need to create efficiency tests
+	 */
+	
+	
 }
